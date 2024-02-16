235bafb82fad440a2fadcbca6f0ea6d6450e6f69
ivanvalor <ivan.valor.verdu@gmail.com>
2024-02-16 16:44:06 +0100

    Added do-while

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,24 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string resp;
+            string cantidad;
+            do
+            {
+                Console.WriteLine("segundos/minutos?");
+                resp = Console.ReadLine();
+                Console.WriteLine("Cantidad a convertir?");
+                cantidad = Console.ReadLine();
+
+                if (resp == "segundos")
+                {
+                    Console.WriteLine(HadaP1.Seconds2Minutes(double.Parse(cantidad)));
+                }
+                else
+                {
+                    Console.WriteLine(HadaP1.Minutes2Seconds(double.Parse(cantidad)));
+                }
+            } while (resp == "segundos" || resp == "minutos");
         }
     }
 }
